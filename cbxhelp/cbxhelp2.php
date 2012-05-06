cfd7363f0ca92e65924065dceb83e1711acba214
Katy Kneiser <kneiserk@gmail.com>
2012-05-06 17:26:17 -0500

    linked to bfhs

---
+++ cbxhelp/cbxhelp2.php
@@ -0,0 +1,38 @@
+<html> 
+
+<head>
+<link rel="stylesheet" href="cbxhelp.css">
+ 
+<title> CBX Helper </title>
+</head>
+
+<body>
+	<div id="body_title_container">
+		<h1> CBX Helper</h1>
+	</div>
+
+	<div id="fronts_reds">
+		<h2>Fronts</h2>
+		<form action="index.php" method="get">
+  			Exported Sheets: <input type="text" required name="frontsreds" /><br />
+  			<input type="submit" value="Compare to Settles" />
+		</form>	
+	</div>
+	
+	<div id="shorts">
+		<h2>Shorts</h2>
+		<form action="index.php" method="get">
+  			Exported Sheets: <input type="text" required name="shorts" /><br />
+  			<input type="submit" value="Compare to Settles" />
+		</form>		
+	</div>
+
+	<div id="greens">
+		<h2>Greens</h2>
+		<form action="index.php" method="get">
+  			Exported Sheets: <input type="text" required name="greens" /><br />
+  			<input type="submit" value="Compare to Settles" />
+		</form>	
+	</div>
+</body>
+</html>
