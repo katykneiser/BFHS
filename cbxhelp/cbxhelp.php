5b3d9a5666f9fb4f1171d9cd5dc08d293d735fd7
Katy Kneiser <kneiserk@gmail.com>
2012-05-06 17:02:32 -0500

    cbxhelp page basic setup started

---
+++ cbxhelp/cbxhelp.php
@@ -0,0 +1,26 @@
+<html> 
+
+<head>
+<link rel="stylesheet" href="cbxhelp.css">
+ 
+<title> CBX Helper </title>
+</head>
+
+<body>
+	<div id="body_title_container">
+		<h1> CBX Helper</h1>
+	</div>
+
+	<div id="Front/Reds">
+		<h2>Fronts</h2>
+	</div>
+	
+	<div id="Shorts">
+		<h2>Shorts</h2>
+	</div>
+
+	<div id="Greens">
+		<h2>Greens</h2>
+	</div>
+</body>
+</html>
